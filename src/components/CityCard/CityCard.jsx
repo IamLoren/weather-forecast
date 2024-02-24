794e7175ce297dc9b7ecdee20c550a0e7ffdd5c4
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-02-24 11:54:00 +0200

    formatted CityCard

--- src/components/CityCard/CityCard.jsx
+++ src/components/CityCard/CityCard.jsx
@@ -1,55 +1,68 @@
-import React from 'react';
-import s from './CityCard.module.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectedCity } from '../../redux/selectors';
-import { changeSelectedCity, clearWeatherInSelectedCity, deleteCityFromListOfTrips } from '../../redux/tripSlice';
-import { fetchAllDays, fetchTodayWeather } from '../../configAxios/operations';
+import React from "react";
+import s from "./CityCard.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { selectedCity } from "../../redux/selectors";
+import {
+  changeSelectedCity,
+  clearWeatherInSelectedCity,
+  deleteCityFromListOfTrips,
+} from "../../redux/tripSlice";
+import { fetchAllDays, fetchTodayWeather } from "../../configAxios/operations";
 
-const CityCard = ({city, startDate, endDate, id, img}) => {
-    const dispatch = useDispatch();
-    const selectedCit = useSelector(selectedCity);
+const CityCard = ({ city, startDate, endDate, id, img }) => {
+  const dispatch = useDispatch();
+  const selectedCit = useSelector(selectedCity);
 
-    const allWeather = async (params) => {
-        const { city } = params;
-        dispatch(changeSelectedCity(params));
-        try {
-          await Promise.all([
-            dispatch(fetchAllDays(params)),
-            dispatch(fetchTodayWeather(city)),
-          ]);
-        } catch (error) {
-          console.error("Error fetching weather:", error);
-        }
-      };
-    
-      const deleteCity = (id) => {
-        dispatch(deleteCityFromListOfTrips(id));
-        if (id === selectedCit.id) {
-          dispatch(changeSelectedCity(''));
-          dispatch(clearWeatherInSelectedCity())
-        }
-      }
+  const allWeather = async (params) => {
+    const { city } = params;
+    dispatch(changeSelectedCity(params));
+    try {
+      await Promise.all([
+        dispatch(fetchAllDays(params)),
+        dispatch(fetchTodayWeather(city)),
+      ]);
+    } catch (error) {
+      console.error("Error fetching weather:", error);
+    }
+  };
+
+  const deleteCity = (id) => {
+    dispatch(deleteCityFromListOfTrips(id));
+    if (id === selectedCit.id) {
+      dispatch(changeSelectedCity(""));
+      dispatch(clearWeatherInSelectedCity());
+    }
+  };
 
   return (
     <li
-            key={id}
-            className={`${s.card} ${id === selectedCit.id ? s.selected : ''}`}
-            onClick={(event) => {
-              if (event.target.tagName !== "BUTTON") {
-                allWeather({ city, startDate, endDate, id });
-              }
-            }}
+      key={id}
+      className={`${s.card} ${id === selectedCit.id ? s.selected : ""}`}
+      onClick={(event) => {
+        if (event.target.tagName !== "BUTTON") {
+          allWeather({ city, startDate, endDate, id });
+        }
+      }}
+    >
+      <img src={img} alt="city" className={s.cardImg} />
+      <div className={s.cardDescription}>
+        <p className={s.cityAnDeleteWrapper}>
+          <span>{city}</span>
+          <button
+            className={s.deleteButton}
+            type="button"
+            onClick={() => deleteCity(id)}
           >
-            <img src={img} alt="city" className={s.cardImg} />
-            <div className={s.cardDescription}>
-              <p className={s.cityAnDeleteWrapper}><span>{city}</span><button className={s.deleteButton} type="button" onClick={()=>deleteCity(id)}> - </button></p>
-              <p>
-                {startDate.replaceAll("-", ".")} -{" "}
-                {endDate.replaceAll("-", ".")}
-              </p>
-            </div>
-          </li>
-  )
-}
+            {" "}
+            -{" "}
+          </button>
+        </p>
+        <p>
+          {startDate.replaceAll("-", ".")} - {endDate.replaceAll("-", ".")}
+        </p>
+      </div>
+    </li>
+  );
+};
 
-export default CityCard
+export default CityCard;
